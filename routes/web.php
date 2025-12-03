63310f73757eb723e712b34bd98b8f5894c31994
Ruslan Khuppatullinov <RyanCrien@gmail.com>
2025-12-04 03:37:53 +0500

    Start project

---
+++ routes/web.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+}); 
+
+Route::get('/hello', function () {
+    return view('hello', ['title' => 'hello world!']);
+}); 
+
