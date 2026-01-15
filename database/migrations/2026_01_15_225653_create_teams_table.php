eec96e3439f11f4644d9f513f632381003e45731
Ruslan Khuppatullinov <RyanCrien@gmail.com>
2026-01-16 04:23:01 +0500

    Migrations task

--- database/migrations/2026_01_15_225653_create_teams_table.php
+++ database/migrations/2026_01_15_225653_create_teams_table.php
@@ -9,19 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('teams', function (Blueprint $table) {
+        $table->id();
+        $table->string('name');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('teams');
     }
 };
